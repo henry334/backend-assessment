4c7eb2821a49da3e76005f5c60a4a97fe589f6ab
henry334 <henry.giraud@epitech.eu>
2024-10-06 17:13:52 +0100

    RM not used decorator

--- task_1/views.py
+++ task_1/views.py
@@ -4,7 +4,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-@api_view(['GET']) # Add GET method to be able to access the view
 def product_list(request):
     products = Product.objects.all()
     if not products.exists():
